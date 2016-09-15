e0ded05b1303be7e832155a16a997116996b32f6
Justin <justin.lee.17@dartmouth.edu>
2016-09-15 16:43:14 -0400

    added translating

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 def hello():
 	if request.method == 'POST':
 		request_data = request.get_json()
-		if 'Show me a gif' in request_data["text"]:
-			requests.post("https://api.groupme.com/v3/bots/post", data = {"bot_id" : "d3822257e9df07545ebfdbbecb", "text" : "https://media.giphy.com/media/14k2L68sg1rVZu/giphy.gif"})
+		if request_data["text"].partition(" ")[0].lower() == "giphy":
+			query = request_data["text"].partition(" ")[2]
+			query.replace(" ", "+")
+			r = requests.get("http://api.giphy.com/v1/gifs/translate", data = {"api_key": "dc6zaTOxFJmzC", "s" : query})
+			gif_url = r.json()["data"]["images"]["original"]["url"]
+			requests.post("https://api.groupme.com/v3/bots/post", data = {"bot_id" : "d3822257e9df07545ebfdbbecb", "text" : gif_url})
 	return "Hello World!"
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-	r = requests.post("http://api.giphy.com/v1/gifs/translate", data = {"api_key": "dc6zaTOxFJmzC", "s" : "south+park"})
-	return r.text
+	r = requests.get("http://api.giphy.com/v1/gifs/translate", data = {"api_key": "dc6zaTOxFJmzC", "s" : "south+park"})
+	return r.json()["data"]["images"]["original"]["url"]
 
 if __name__ == "__main__":
 	app.run()
