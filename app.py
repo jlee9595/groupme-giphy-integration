606a80c90a8b3c6114483f8f407194381a215201
Justin <justin.lee.17@dartmouth.edu>
2016-09-15 17:49:47 -0400

    Style changes

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, request
 import requests
+
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-	if request.method == 'POST':
-		request_data = request.get_json()
-		if request_data["text"].partition(" ")[0].lower() == "giphy":
-			query = request_data["text"].partition(" ")[2]
+GIPHY_API_KEY = "dc6zaTOxFJmzC"
+BOT_ID = "d3822257e9df07545ebfdbbecb"
+GIPHY_ENDPOINT = "http://api.giphy.com/v1/gifs/translate"
+GROUPME_ENDPOINT = "https://api.groupme.com/v3/bots/post"
+
+@app.route("/", methods=["GET", "POST"])
+def send_gif():
+	if request.method == "POST":
+		text = request.get_json()["text"]
+
+		if text.partition(" ")[0].lower() == "giphy":
+			query = text.partition(" ")[2]
 			query.replace(" ", "+")
-			r = requests.get("http://api.giphy.com/v1/gifs/translate", data = {"api_key": "dc6zaTOxFJmzC", "s" : query})
-			gif_url = r.json()["data"]["images"]["original"]["url"]
-			requests.post("https://api.groupme.com/v3/bots/post", data = {"bot_id" : "d3822257e9df07545ebfdbbecb", "text" : gif_url})
-	return "Hello World!"
+
+			r = requests.get(GIPHY_ENDPOINT, data = {"api_key": GIPHY_API_KEY, "s" : query})
+			gif_url = r.json()["data"]["images"]["downsized_large"]["url"]
+			response = gif_url if gif_url != None else "Sorry I could not find a gif for that"
+
+			requests.post(GROUPME_ENDPOINT, data = {"bot_id" : BOT_ID, "text" : response})
+
+	return "gif sent"
 
 if __name__ == "__main__":
 	app.run()
