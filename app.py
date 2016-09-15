e214302a5aa6035c95aaa95b275cc47a9b91fbc2
Justin <justin.lee.17@dartmouth.edu>
2016-09-15 15:06:27 -0400

    added callback

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, request
 import requests
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-	requests.post("https://api.groupme.com/v3/bots/post", data = {"bot_id" : "d3822257e9df07545ebfdbbecb", "text" : "https://media.giphy.com/media/14k2L68sg1rVZu/giphy.gif"})
+	if request.method == 'POST':
+		request_data = request.get_json()
+		if 'Show me a gif' in request_data["text"]:
+			requests.post("https://api.groupme.com/v3/bots/post", data = {"bot_id" : "d3822257e9df07545ebfdbbecb", "text" : "https://media.giphy.com/media/14k2L68sg1rVZu/giphy.gif"})
 	return "Hello World!"
 
 if __name__ == "__main__":
