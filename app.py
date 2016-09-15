42b0eb47573abc2c36f7192a1792709d87941d7a
Justin <justin.lee.17@dartmouth.edu>
2016-09-15 14:42:22 -0400

    testing callback

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	requests.post("https://api.groupme.com/v3/bots/post", data = {"bot_id" : "d3822257e9df07545ebfdbbecb", "text" : "https://media.giphy.com/media/14k2L68sg1rVZu/giphy.gif"})
+	return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
